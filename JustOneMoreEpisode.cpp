bc8acc42deada8255bc6ec0667d1a5e985edc9da
Udit Kumar <58100192+kudit223@users.noreply.github.com>
2023-03-09 00:57:14 +0530

    Create JustOneMoreEpisode.cpp

---
+++ JustOneMoreEpisode.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes herif(x)
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x;
+	    cin>>x;
+	    if(x-24>0)
+	    cout<<"yes";
+	    else
+	    cout<<"no";
+	    cout<<'\n';
+	}
+	return 0;
+}
