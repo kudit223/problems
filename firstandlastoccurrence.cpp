8aa083e533908ac283b9541a4350f657d9787730
Udit Kumar <58100192+kudit223@users.noreply.github.com>
2023-03-25 20:30:01 +0530

    Update firstandlastoccurrence.cpp

--- firstandlastoccurrence.cpp
+++ firstandlastoccurrence.cpp
@@ -27,3 +27,34 @@ int main()
     first(v,0,n-1,k,maxx);
     cout<<maxx;
 }
+-----------------------------------------------------------------------***********--------------
+    //using return type int
+    //first and last occurrence of element
+#include<bits/stdc++.h>
+using namespace std;
+
+int first(vector<int> v,int s,int e,int k,int &result)
+{
+    int mid=s+(e-s)/2;
+    if(s>e)
+    return result;
+    if(v[mid]==k)
+    result=mid;
+    if(v[mid]>k)                //if(v[mid]>=k)
+    first(v,s,mid-1,k,result);
+    else
+    first(v,mid+1,e,k,result);
+
+}
+
+int main()
+{
+    int n,k;
+    cin>>n>>k;
+    vector<int>v(n);
+    for(int i=0;i<n;i++)
+    cin>>v[i];
+    int maxx=0;
+    first(v,0,n-1,k,maxx);
+    cout<<maxx;
+}
